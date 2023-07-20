25463d271f2f352ae16b7e4d0194a1240e522195
saswatparida16 <115345561+saswatparida16@users.noreply.github.com>
2023-07-20 14:05:08 +0530

    Update body.js

--- body.js
+++ body.js
@@ -34,3 +34,50 @@ nextButton.addEventListener("click", handleNextButtonClick);
 // Show the initial slide
 showSlide(currentIndex);
 
+// for product
+
+// Simulated list of products (You can replace this with actual data fetched from a server or API)
+const products = [
+  {
+    name: "Watch Model 1",
+    image: "https://media-assets.hyperinvento.com/companies/917dd2ad-9461-4902-9d36-3760dd11ad56/others/others/files/7f5810a638a44cb3b4fefa330f000ed0-other-other.jpg",
+    price: 199.99,
+  },
+  {
+    name: "Watch Model 2",
+    image: "https://media-assets.hyperinvento.com/companies/917dd2ad-9461-4902-9d36-3760dd11ad56/others/others/files/7f5810a638a44cb3b4fefa330f000ed0-other-other.jpg",
+    price: 249.99,
+  },
+  {
+    name: "Watch Model 3",
+    image: "https://media-assets.hyperinvento.com/companies/917dd2ad-9461-4902-9d36-3760dd11ad56/others/others/files/7f5810a638a44cb3b4fefa330f000ed0-other-other.jpg",
+    price: 179.99,
+  },
+  // Add more product items here
+];
+
+// Function to dynamically generate product items and display them on the page
+function displayProducts() {
+  const watchList = document.getElementById("watchList");
+  let productHTML = "";
+
+  products.forEach((product) => {
+    productHTML += `
+      <div class="watch-item">
+        <img src="${product.image}" alt="${product.name}">
+        <h3>${product.name}</h3>
+        <p class="watch-price">$${product.price.toFixed(2)}</p>
+      </div>
+    `;
+  });
+
+  watchList.innerHTML = productHTML;
+}
+
+// Call the function to display products when the page loads
+document.addEventListener("DOMContentLoaded", displayProducts);
+
+
+// Show the initial slide
+showSlide(currentIndex);
+
